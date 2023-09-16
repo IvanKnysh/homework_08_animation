53042fdf55a3760eb79b371eff3500364f7c9d85
Ivan Knysh <mr.rauber.ik@gmail.com>
2023-09-16 16:49:38 +0300

    Clouds with parallax added

--- cartoon/js/index.js
+++ cartoon/js/index.js
@@ -57,3 +57,19 @@ document.querySelector("#close").addEventListener("click", () => {
 	document.querySelector(".warning").classList.remove("active");
 	document.querySelector(".overlay").classList.remove("active");
 });
+
+/**
+ * Parallax
+ */
+const root = document.documentElement;
+
+root.addEventListener("mousemove", (e) => {
+	root.style.setProperty(
+		"--move-x",
+		(e.clientX - innerWidth / 2) * -0.005 + "deg"
+	);
+	root.style.setProperty(
+		"--move-y",
+		(e.clientY - innerHeight / 2) * -0.01 + "deg"
+	);
+});
